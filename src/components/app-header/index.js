29f985a0240a9c1d5af0d33ac5617443d0000903
amira-s <amira_s@etna-alternance.net>
2017-05-31 12:58:30 +0200

    [refactor]: fix .editorconfig and cleanup code

--- src/components/app-header/index.js
+++ src/components/app-header/index.js
@@ -3,13 +3,12 @@ import logo from 'assets/millennium-falcon.svg';
 import './styles.css';
 
 const AppHeader = () => {
-
-        return (
-                <div className="app-header stars">
-                    <img src={logo} className="app-logo" alt="logo" />
-                    <h2>Jedi List</h2>
-                </div>
-            );
+    return (
+        <div className="app-header stars">
+            <img src={logo} className="app-logo" alt="logo" />
+            <h2>Jedi List</h2>
+        </div>
+    );
 };
 
 export default AppHeader;
